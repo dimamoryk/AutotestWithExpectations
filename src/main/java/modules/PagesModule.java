49c7f7345ddfbb40df48bf73af9beea67dcb6a33
dimamoryk <dimamoryk@gmail.com>
2025-12-05 21:41:07 +0300

    Update PagesModule.java

--- src/main/java/modules/PagesModule.java
+++ src/main/java/modules/PagesModule.java
@@ -1,4 +1,25 @@
 package modules;
 
-public class PagesModule {
+import com.google.inject.AbstractModule;
+import components.CatalogCoursesComponent;
+import components.popups.AuthPopup;
+import org.openqa.selenium.WebDriver;
+import pages.CategoriesPage;
+import pages.MainPage;
+
+public class PagesModule extends AbstractModule {
+
+    protected WebDriver driver;
+
+    public PagesModule(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    protected void configure() {
+        bind(MainPage.class).toInstance(new MainPage(driver));
+        bind(CategoriesPage.class).toInstance(new CategoriesPage(driver));
+        bind(AuthPopup.class).toInstance(new AuthPopup(driver));
+        bind(CatalogCoursesComponent.class).toInstance(new CatalogCoursesComponent(driver));
+    }
 }
